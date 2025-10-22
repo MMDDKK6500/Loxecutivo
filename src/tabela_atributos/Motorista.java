b4ea8ad66f881c0c5bbc42f72d1f5871e4131615
Hazel Borim <millieborim@gmail.com>
2025-10-21 22:20:59 -0300

    Ajustes nos atributos

--- src/tabela_atributos/Motorista.java
+++ src/tabela_atributos/Motorista.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tabela_atributos;
 
-/**
- *
- * @author hazel
- */
 public class Motorista {
-    
+    private int id_motorista;
+    private String nome;
+    private String sobrenome;
+    private String rg;
+    private String cpf;
+
+    public int getId_Motorista() {
+        return id_motorista;
+    }
+
+    public void setId_Motorista(int id_motorista) {
+        this.id_motorista = id_motorista;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRG() {
+        return rg;
+    }
+
+    public void setRG(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCPF() {
+        return cpf;
+    }
+
+    public void setCPF(String cpf) {
+        this.cpf = cpf;
+    }
 }
--- src/tabela_atributos/Veiculo.java
+++ src/tabela_atributos/Veiculo.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tabela_atributos;
 
-/**
- *
- * @author hazel
- */
 public class Veiculo {
-    
+    private int id_placa;
+    private String modelo;
+    private int ano;
+    private String marca;
+    private String cor;
+
+    public int getId_Placa() {
+        return id_placa;
+    }
+
+    public void setId_Placa(int id_placa) {
+        this.id_placa = id_placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 }
